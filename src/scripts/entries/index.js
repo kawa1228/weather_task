f23bafef508bdf5626b870ff71341f43203a0ecc
kawa1228 <sxa.1105bo@gmail.com>
2018-09-14 11:34:28 +0900

    weatherAryの削除

--- src/scripts/entries/index.js
+++ src/scripts/entries/index.js
@@ -1,20 +1,7 @@
 import axios from 'axios'
 
 import * as forecasts from '../weather_data'
-
-class weatherAry {
-  makeAry() {
-    const weatherData = forecasts.weatherData()
-    const newAry = []
-
-    for (let i = 0; i < weatherData.length; i++) {
-      newAry.push(weatherData[i])
-    }
-    return newAry
-  }
-}
-
-const getData = new weatherAry().makeAry()
+const weatherData = forecasts.weatherData()
 
 class Interval {
   execute() {
@@ -25,10 +12,10 @@ class Interval {
     return setInterval(this.turnData, 2000)
   }
   turnData() {
-    let shiftData = getData.shift()
+    let shiftData = weatherData.shift()
 
-    getData.push(shiftData)
-    console.log(getData)
+    weatherData.push(shiftData)
+    console.log(weatherData)
   }
   stopInterval(id) {
     document.getElementById('stop').addEventListener('click', () => {
