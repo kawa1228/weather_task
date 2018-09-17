5b7d9f64a17ae8fe5a54f165edec1bde50b3269d
kawa1228 <sxa.1105bo@gmail.com>
2018-09-18 00:07:48 +0900

    途中

--- src/scripts/entries/index.js
+++ src/scripts/entries/index.js
@@ -1,31 +1,73 @@
+import * as display from '../display'
 import $ from "jquery";
 import 'slick-carousel';
 import axios from 'axios'
 
-$('.weather-wrapper').slick({
-  arrows: false,
-  autoplay: true,
-  speed: 2000
-});
+class Test {
 
-function tokyo() {
-  getWeather(130010).then((res) => {
-    console.log(res)
-  })
-}
-tokyo()
+  constructor() {
+    // Promise.then(this.putData()).then(() => this.addSlick())
+    this.putData()
+    setTimeout(() => this.addSlick(), 1000)
+  }
 
-function kanagawa() {
-  getWeather(140010).then((res) => {
-    console.log(res)
-  })
-}
-kanagawa()
+  getWeather(cityId) {
+    return axios.get(`http://localhost:8001/sample.php?city_id=${cityId}`).then((res) => {
+      return res.data
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
-function getWeather(cityId) {
-  return axios.get(`http://localhost:8001/sample.php?city_id=${cityId}`).then((res) => {
-    return res.data
-  }).catch((err) => {
-    console.log(err)
-  })
+  putData() {
+    const app = document.createElement('div')
+    app.className = 'app'
+
+    // const area = 'とうきょう'
+    // const telop = 'hoge'
+    // const image = ''
+    // const tokyo = new display.MakeDisplay().makeElm(area, telop, image)
+    // const kanagawa = new display.MakeDisplay().makeElm(area, telop, image)
+    // app.appendChild(tokyo)
+    // app.appendChild(kanagawa)
+
+    //東京
+    this.getWeather(130010).then((res => {
+      const area = res.location.prefecture
+      const telop = res.forecasts[0].telop
+      const image = res.forecasts[0].image.url
+
+      const tokyo = new display.MakeDisplay().makeElm(area, telop, image)
+      app.appendChild(tokyo)
+    }))
+
+    //神奈川
+    this.getWeather(140010).then((res => {
+      const area = res.location.prefecture
+      const telop = res.forecasts[0].telop
+      const image = res.forecasts[0].image.url
+
+      const kanagawa = new display.MakeDisplay().makeElm(area, telop, image)
+      app.appendChild(kanagawa)
+    }))
+
+    const result = document.getElementById('result')
+    result.appendChild(app)
+  }
+
+  addSlick() {
+    const elem = document.querySelector('.app')
+
+    $(elem).slick({
+      arrows: false,
+      autoplay: true,
+      speed: 2000
+    });
+  }
 }
+const test = new Test()
+
+// test.addSlick()
+// putData()
+
+// Promise.resolve().then(putData()).then(addSlick())
